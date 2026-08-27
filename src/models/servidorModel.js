10b40d4fb3459b45c87b3e9e2c0f38a54d9233f2
Lucas-Galvao07 <lucas.galvao@sptech.school>
2026-08-26 21:42:35 -0300

    Feat: alteração de tabelas e regra de negócio do banco e adaptação nas models

--- src/models/servidorModel.js
+++ src/models/servidorModel.js
@@ -6,7 +6,7 @@ function buscarServidorId(id) {
 }
 
 function inserirServidor(nome, fk_empresa) {
-    var instrucaoSql = `INSERT INTO dashboard (nome, fk_empresa) VALUES ('${nome}', ${fk_empresa})`;
+    var instrucaoSql = `INSERT INTO servidor (nome, fk_empresa) VALUES ('${nome}', ${fk_empresa})`;
 
     return database.executar(instrucaoSql)
 }
@@ -17,15 +17,31 @@ function listarPorEmpresa(fk_empresa) {
 }
 
 function excluirDash(id) {
-    var instrucaoSql = `DELETE FROM dashboard WHERE id = ${id};`;
+    var instrucaoSql = `DELETE FROM servidor WHERE id = ${id};`;
+    return database.executar(instrucaoSql);
+}
+
+function aviso(nome, id) {
+    var instrucaoSql = `
+    SELECT
+        e.nome_fantasia,
+        s.nome AS servidor,
+        c.valor AS cpu
+    FROM empresa e
+    JOIN servidor s
+        ON e.id = s.fk_empresa
+    JOIN captura c
+        ON s.id = c.fk_servidor
+    WHERE c.nome = ${nome} and s.id = ${id}
+    AND c.valor > 90;`
     return database.executar(instrucaoSql);
 }
 
 function buscarDashComEmpresa(id) {
-     var instrucaoSql = `SELECT d.id, e.nome_fantasia
-     FROM dashboard d
-     JOIN empresa e on e.fk_empresa = d.id 
-     WHERE d.id = ${id}`;
+     var instrucaoSql = `SELECT s.id, e.nome_fantasia
+     FROM servidor s
+     JOIN empresa e on e.id = s.fk_empresa 
+     WHERE s.id = ${id}`;
 
      return database.executar(instrucaoSql)
 }
